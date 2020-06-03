122210c1ade9a73048e18afbf7e84afa54095f20
Ninjosh2 <ninjosh2@gmail.com>
2020-06-03 17:32:12 -0500

    working with my sale_entries block

--- the-apollo-marketplace/app/controllers/sale_entries_controller.rb
+++ the-apollo-marketplace/app/controllers/sale_entries_controller.rb
@@ -8,8 +8,22 @@ class SaleEntriesController < ApplicationController
 
 
 
-    post '/' do
-        
+    post '/sale_entries' do
+
+        if !logged_in?
+            redirect '/'
+        end
+
+        if params[:sale_entries] !=''
+            @sale_entry = SaleEntry.create(item: params[:item], description: params[:description], price: params[:price], user_id: current_user.id)
+            redirect "/sale_entries/#{@sale_entry.id}"
+        else
+            redirect '/sale_entries/new'
+        end
+
+
+
+
     end
    
 
