2d95074c4d8efd94bc1ab5a43b8e1a9488197ebc
Ninjosh2 <ninjosh2@gmail.com>
2020-06-02 23:46:10 -0500

    modified the application and user controller. also added login

--- the-apollo-marketplace/app/controllers/users_controller.rb
+++ the-apollo-marketplace/app/controllers/users_controller.rb
@@ -6,10 +6,30 @@ class UsersController < ApplicationController
         
     end
 
+
+    #creates a session for the user that's logged in.
+    post '/login' do
+        @user = User.find_by(email: params[:email])
+        
+        if @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            puts session
+            redirect "users/#{@user.id}"
+            
+        else
+            
+        end
+        
+    end
+
     #the signup route is here!
 
     get '/signup' do
         
+    end
+
+    get '/users/:id' do
+        "user page"
     end
 
 end
