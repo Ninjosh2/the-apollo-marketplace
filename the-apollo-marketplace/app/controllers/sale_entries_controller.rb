69a4ea3f02531b3b2fb7dd444ed1f83c9ca0a787
Ninjosh2 <ninjosh2@gmail.com>
2020-06-03 16:56:36 -0500

    added a sales controller with a view

---
+++ the-apollo-marketplace/app/controllers/sale_entries_controller.rb
@@ -0,0 +1,17 @@
+class SaleEntriesController < ApplicationController
+
+    get '/sale_entries/new' do
+        erb :'/sale_entries/new'
+        
+    end
+
+
+
+
+    post '/' do
+        
+    end
+   
+
+
+end
