2f52a8371240a5221cc8e0b73e732d6967bfcfea
Ninjosh2 <ninjosh2@gmail.com>
2020-06-02 20:27:27 -0500

    built a user controller and added to config.ru

---
+++ the-apollo-marketplace/app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+
+    #the login route I want is here
+    get '/login' do
+        erb :login
+        
+    end
+
+    #the signup route is here!
+
+    get '/signup' do
+        
+    end
+
+end
