5d5fa627c90c02cd8f7962debfe3eeac53a17897
Ninjosh2 <ninjosh2@gmail.com>
2020-06-03 23:22:29 -0500

    added an edit route and worked on the show page

--- the-apollo-marketplace/app/controllers/sale_entries_controller.rb
+++ the-apollo-marketplace/app/controllers/sale_entries_controller.rb
@@ -2,11 +2,7 @@ class SaleEntriesController < ApplicationController
 
     get '/sale_entries/new' do
         erb :'/sale_entries/new'
-        
     end
-
-
-
 
     post '/sale_entries' do
 
@@ -14,18 +10,23 @@ class SaleEntriesController < ApplicationController
             redirect '/'
         end
 
-        if params[:sale_entries] !=''
+        if params[:item] != "" 
             @sale_entry = SaleEntry.create(item: params[:item], description: params[:description], price: params[:price], user_id: current_user.id)
             redirect "/sale_entries/#{@sale_entry.id}"
         else
             redirect '/sale_entries/new'
         end
-
-
-
-
     end
-   
 
+        #show route for sale entry
+    get '/sale_entries/:id' do
+        @sale_entry = SaleEntry.find(params[:id])
+        erb :'/sale_entries/show'
+    end
+
+    #this will send us to the sale_entries/edit erb, and have #an edit form
+    get '/sale_entries/:id/edit' do
+        erb :'/sale_entries/edit'
+      end
 
 end
