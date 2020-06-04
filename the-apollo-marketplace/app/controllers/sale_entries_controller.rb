6ffbf5d83351151a38777e4ba56a2fce64b26d42
Ninjosh2 <ninjosh2@gmail.com>
2020-06-04 17:53:15 -0500

    modded the sale_entries controller

--- the-apollo-marketplace/app/controllers/sale_entries_controller.rb
+++ the-apollo-marketplace/app/controllers/sale_entries_controller.rb
@@ -20,13 +20,41 @@ class SaleEntriesController < ApplicationController
 
         #show route for sale entry
     get '/sale_entries/:id' do
-        @sale_entry = SaleEntry.find(params[:id])
+        set_sale_entry
         erb :'/sale_entries/show'
     end
 
     #this will send us to the sale_entries/edit erb, and have #an edit form
     get '/sale_entries/:id/edit' do
-        erb :'/sale_entries/edit'
+        set_sale_entry
+        if logged_in?
+            if @sale_entry.user == current_user
+                erb :'/sale_entries/edit'
+            else
+                redirect "users/#{current_user.id}"
+             end  
+        else
+            redirect '/'
+        end 
+    end
+
+      patch '/sale_entries/:id' do
+        set_sale_entry
+        if logged_in?
+            if @sale_entry.user == current_user
+                @sale_entry.update(item: params[:item], description: params[:description], price: params[:price])
+                redirect "/sale_entries/#{@sale_entry.id}"
+            else
+                redirect "users/#{current_user.id}"
+            end
+        else
+            redirect '/'
+        end
+      end
+
+      private
+      def set_sale_entry
+        @sale_entry = SaleEntry.find(params[:id])
       end
 
 end
